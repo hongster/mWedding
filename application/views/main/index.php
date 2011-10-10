dfab8f1b01968bcc25eea6c09cfadeea150f7320
Leong Hean Hong <hong@mrleong.net>
2011-10-11 02:02:01 +0800

    Design for frontpage.

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,29 +1,52 @@
-<p>
-	<?php echo Form::open('main/search'); ?>
-	<?php echo Form::input('query', ''); ?><?php echo Form::submit('', 'Search'); ?>
-	<?php echo Form::close(); ?>
-</p>
+<section id="frontpage-banner" class="grid_12">
+	<?php echo HTML::image('img/banner.jpg', array('alt'=>'banner')); ?>
+</section>
+<div class="clear"></div>
 
-<p>
-	<?php echo HTML::anchor('table/add', 'Add Table'); ?>
-</p>
-
-<?php foreach ($tables as $table): ?>
-	<table border="1">
-		<tr>
-			<th>
-				<?php echo HTML::anchor('/table/update/'.$table->id, $table->name); ?>
-				[<?php echo HTML::anchor('/table/delete/'.$table->id, 'Delete'); ?>]
-			</th>
-		</tr>
+<section>
+	<article class="grid_6" style="background-color:#fefefe">
+		<h2>What is mWedding?</h2>
 		
-		<?php foreach ($table->all_guests() as $guest): ?>
-			<tr>
-				<td style="background-color:<?php echo $guest->has_arrived() ? '#66FF66' : '#EFEFEF';?>">
-					<?php echo HTML::anchor('/guest/info/'.$guest->id, $guest->name); ?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-	</table>
-	<br />
-<?php endforeach; ?>
+		<p>This is a wedding guests attendance marking system. It is deisgned
+		to solve some of the problems related to organizing a wedding 
+		dinner. The wedding couples needs to know the number of guests 
+		who have turned up, so that dinner commences when majority
+		of the invited guests have arrived. When a guest arrived, there is a need
+		to efficiently determine where he/she sits, and mark attendance. The
+		banquet manager needs to be informed on the number of guests
+		with special diet requirements on a timely manner. Keeping track 
+		of such information has never been an easy task.
+		</p>
+		
+		<p>mWedding solves these problems be allow wedding organizer to
+		allocate guests to tables. With a built-in search function, information
+		such as dietary needs, seating assignment can be easily retireved.
+		Attandence can be marked this information is made available realtime.
+		</p>
+	</article>
+	<!-- what is mWedding -->
+	
+	<article class="grid_6" style="background-color:#fefefe">
+		<h2>How does it works?</h2>
+		
+		<h3>Pre Dinner Day</h3>
+		<ol>
+			<li>Define tables and assign name/numbering to them.</li>
+			<li>Add guests to each table.</li>
+			<li>Optional, tag each guest with <em>#hashtag</em>.</li>
+		</ol>
+		
+		<h3>Dinner Day</h3>
+		<ol>
+			<li>When guest arrived, search by name.</li>
+			<li>Inform guest the assigned table.</li>
+			<li>Checkin the arrived guest.</li>
+			<li>Update banquet manager number of guests with special diet needs.</li>
+			<li>Start dinner when enough guests have arrived.</li>
+		</ol>
+		
+		<p id="get-started"><a href="#" class="button">Get Started</a></p>
+	</article>
+	<!-- howto -->	
+	<div class="clear"></div>
+</section>
--- application/views/template.php
+++ application/views/template.php
@@ -1,18 +1,34 @@
 <!DOCTYPE HTML>
-<html>
+<html xmlns:ng="http://angularjs.org">
 <head>
-	<title>Zhen Hao &amp; Janice Wedding</title>
+	<title>mWedding</title>
+	
+	<?php echo HTML::style('css/reset.css'); ?>
+	<?php echo HTML::style('css/text.css'); ?>
+	<?php echo HTML::style('css/960.css'); ?>
+	<?php echo HTML::style('css/style.css'); ?>
+	<?php echo HTML::style('css/jquery-ui-1.8.16.custom.css'); ?>
+	
+	<script src="http://code.angularjs.org/0.9.15/angular-0.9.15.min.js" ng:autobind></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+	<?php echo HTML::script('js/jquery-ui-1.8.16.custom.min.js'); ?>
 </head>
 
 <body>
-	<header>
-		<nav>
-			<?php echo HTML::anchor('', 'Tables'); ?>
-		</nav>
+	<header class="container_12">
+		<section class="grid_12">
+			<h1 id="site-title">mWedding</h1>
+			<em>Your wedding assistant</em>
+		</section>
 	</header>
 	
-	<?php if(isset($content)) echo $content; ?>
+	<section id="content" class="container_12">
+		<?php if(isset($content)) echo $content; ?>
+	</section>
+	<!-- #content -->
 	
-	<footer></footer>
+	<footer class="container_12">
+		(CC) BY | <a href="http://about.me/hongster">@hongster</a>
+	</footer>
 </body>
 </html>
